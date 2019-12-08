a2e6b92f49641258d1702d2994fcdc3a4111d646
Ty Ridings <tyridings@gmail.com>
2019-12-08 10:32:33 -0700

    Adding the fibonnacci code that prints first 100 fib #'s

---
+++ fibonnacci.py
@@ -0,0 +1,18 @@
+# fibonacci.py
+# Ty Ridings
+# CSCI 102 - Section C
+
+def fib():
+    fibs = [1, 2]
+    Fn = 3
+    for i in range(1,100):
+        Fn = fibs[i - 1] + fibs[i]
+        fibs.append(Fn)
+
+    return fibs
+
+def main():
+    print('OUTPUT', fib())
+
+if __name__ == "__main__":
+    main()
